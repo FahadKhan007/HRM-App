040183b0ed0176c9437f14c10e7a0430790227b6
FahadKhan007 <fahad.bauet@gmail.com>
2023-01-28 04:18:50 +0600

    attendence page design

--- lib/attendance_page.dart
+++ lib/attendance_page.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AttendancePage extends StatefulWidget {
@@ -17,20 +20,109 @@ class _AttendancePageState extends State<AttendancePage> {
       appBar: AppBar(
         backgroundColor: Colors.amberAccent,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              '20:00:01',
-              style: GoogleFonts.ubuntu(
-                fontSize: 20,
-                color: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                '28 Jan 2023',
+                style: GoogleFonts.ubuntu(
+                  fontSize: 20,
+                  color: Colors.black87,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
             ),
-          ),
-        ],
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                '20:00:01',
+                style: GoogleFonts.ubuntu(
+                  fontSize: 20,
+                  color: Colors.black26,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      Get.to(() => AttendancePage());
+                    },
+                    child: Container(
+                      height: 60,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        // ignore: prefer_const_literals_to_create_immutables
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 10,
+                            offset: Offset(2, 2),
+                          ),
+                        ],
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Check In",
+                          style: GoogleFonts.ubuntu(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 80),
+                Container(
+                  alignment: Alignment.centerLeft,
+                  child: GestureDetector(
+                    onTap: () {
+                      Get.to(() => AttendancePage());
+                    },
+                    child: Container(
+                      height: 60,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        // ignore: prefer_const_literals_to_create_immutables
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 10,
+                            offset: Offset(2, 2),
+                          ),
+                        ],
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                      ),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "Check Out",
+                          style: GoogleFonts.ubuntu(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
